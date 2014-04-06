ef229662d5b9b4601037f720f206e540d4528d4f
nscarcella <nscarcella@gmail.com>
2014-04-06 17:21:45 -0300

    optional arguments to chord. Updated README

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,46 +1,55 @@
 'use strict';
 
-var fretCount = 5;
 var stringCount = 6;
-var stringDistance = 16 * 0.5;
-var fretDistance = 16 * 0.75;
-var airMargin = 5;
+var defaultFretCount = 5;
+var defaultStringDistance = 16;
+var defaultFretDistance = 20;
+var defaultAirMargin = 5;
 
 angular.module('myApp.directives', [])
   .directive('cmChord', function(){ return {
     restrict: 'AE',
     templateUrl: 'partials/chord.html',
     scope: {
-      key:'='
+      note:'=',
+      fretCount:'=?',
+      stringDistance:'=?',
+      fretDistance:'=?',
+      airMargin:'=?'
     },
     link: function(scope) {
-      var stringCoord = function(string){ return stringDistance + stringDistance * string };
-      var fretCoord = function(fret){ return scope.pressSize * 2 + airMargin + fretDistance * fret };
+      scope.fretCount = scope.fretCount || defaultFretCount;
+      scope.stringDistance = scope.stringDistance || defaultStringDistance;
+      scope.fretDistance = scope.fretDistance || defaultFretDistance;
+      scope.airMargin = scope.airMargin || defaultAirMargin;
+      
+      var stringCoord = function(string){ return scope.stringDistance + scope.stringDistance * string };
+      var fretCoord = function(fret){ return scope.pressSize * 2 + scope.airMargin + scope.fretDistance * fret };
 
-      scope.pressSize = Math.min(fretDistance,stringDistance) / 4 + 1;
-      scope.width = stringDistance + stringCount * stringDistance;
-      scope.height = scope.pressSize * 3 + airMargin + (fretCount -1) * fretDistance;
-      scope.baseFretX = stringCoord(0) - stringDistance / 2;
-      scope.baseFretY = fretCoord(0) + fretDistance / 2;
+      scope.pressSize = Math.min(scope.fretDistance,scope.stringDistance) / 4 + 1;
+      scope.width = scope.stringDistance + stringCount * scope.stringDistance;
+      scope.height = scope.pressSize * 3 + scope.airMargin + (scope.fretCount -1) * scope.fretDistance;
+      scope.baseFretX = stringCoord(0) - scope.stringDistance / 2;
+      scope.baseFretY = fretCoord(0) + scope.fretDistance / 2;
       
       scope.lines = [];
-      for(var j=0; j<fretCount; j++) scope.lines.push({x1:stringCoord(0), y1:fretCoord(j), x2:stringCoord(stringCount - 1), y2:fretCoord(j)});
-      for(var i=0; i<stringCount; i++) scope.lines.push({x1:stringCoord(i), y1:fretCoord(0), x2:stringCoord(i), y2:fretCoord(fretCount - 1)});
+      for(var j=0; j<scope.fretCount; j++) scope.lines.push({x1:stringCoord(0), y1:fretCoord(j), x2:stringCoord(stringCount - 1), y2:fretCoord(j)});
+      for(var i=0; i<stringCount; i++) scope.lines.push({x1:stringCoord(i), y1:fretCoord(0), x2:stringCoord(i), y2:fretCoord(scope.fretCount - 1)});
       
       scope.coords = [];
       scope.$watch("currentVariation",function(newValue){
         for(var i=0; i<stringCount; i++) {
-          var position = scope.key.variations[scope.currentVariation].positions[i];
+          var position = scope.note.variations[scope.currentVariation].positions[i];
           scope.coords[i] = {
             pressed: position != null,
             x: stringCoord(i),
-            y: position > 0 ? fretCoord(position) - fretDistance / 2 : scope.pressSize
+            y: position > 0 ? fretCoord(position) - scope.fretDistance / 2 : scope.pressSize
           };
         }
       });
       scope.currentVariation = 0;
       
-      scope.setNextVariation = function(){ if(scope.currentVariation + 1 < scope.key.variations.length) scope.currentVariation++; }
+      scope.setNextVariation = function(){ if(scope.currentVariation + 1 < scope.note.variations.length) scope.currentVariation++; }
       scope.setPreviousVariation = function(){ if(scope.currentVariation > 0) scope.currentVariation--; }
     }
   }});
